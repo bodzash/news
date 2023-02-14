6598dd994239e0baa534b083fa6e6b148a559853
bodzash <bozzaylaszlo4@gmail.com>
2023-02-14 11:15:53 +0100

    Added Article style file and installed time pkg

--- src/components/Article.tsx
+++ src/components/Article.tsx
@@ -1,24 +1,31 @@
 import React from "react";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+import "./Article.scss";
 
 export interface ArticleProps {
   title: string | null;
   content: string | null;
   description: string | null;
   image_url: string | null;
+  source_id: string | null;
   pubDate: string;
 }
 
+TimeAgo.addDefaultLocale(en);
+const timeAgo = new TimeAgo("en-US");
+
 export const Article = (props: ArticleProps): JSX.Element => {
   return (
-    <article>
-      <h4>{props.title}</h4>
-      <p>{props.pubDate}</p>
-      <p>{props.description}</p>
+    <article className="article-wrapper">
+      <h4 className="article-title">{props.title}</h4>
+      <p className="article-date">
+        {timeAgo.format(new Date(props.pubDate))} by {props.source_id}
+      </p>
+      <p className="article-desc">{props.description}</p>
       <img
-        src={
-          props.image_url ??
-          "https://placehold.jp/3d4070/ffffff/512x512.png?text=No%20image"
-        }
+        className="article-image"
+        src={props.image_url}
         alt={/*description*/ ""}
       />
     </article>
